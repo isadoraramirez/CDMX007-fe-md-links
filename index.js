d9698ffa31fd448bdae3a7918cd10c6c979dd417
doctoramalvada <mydemoni@hotmail.com>
2019-03-27 20:13:38 -0600

    añado función require fs

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-module.exports = () => {
-   // aqui va algo...
-};
-const http = require('http');
+//module.exports = () => {};
+// const http = require('http');
+
+
+const fs = require('fs');
+// acceder a sistema de ficheros para leer archivo de texto
+fs.readFile('README.md', 'utf-8', (err, data) => {
+  if(err) {
+    console.log('error: ', err);
+  } else {
+    console.log(data);
+  }
+});
+// accede al fichero para lectura y lo devuelve uen string
+
+
+// let links = document.links;
+// for(let i = 0; i < links.length; i++) {
+//   let linkHref = document.createTextNode(links[i].href);
+//    lineBreak = document.createElement("br");
+//   document.body.appendChild(linkHref);
+//   document.body.appendChild(lineBreak);
+//   console.log(linkHref);
+// }
+//debolver una coleccion de los elementos con atributo href
