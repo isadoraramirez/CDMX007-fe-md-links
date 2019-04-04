c2adc06711a41223894312c3a27e8732b130f84d
doctoramalvada <mydemoni@hotmail.com>
2019-04-03 18:35:46 -0600

    validate da true, no pinta arreglo

--- index.js
+++ index.js
@@ -9,209 +9,33 @@ module.exports = () => {};
          fs.readFile(`./${mdExtend}`, 'utf8', (err,fileMd) =>
               {
                   if(fileMd)
-                      console.log(fileMd);
+                      console.log(true);
                   else
                       console.log(err);
 
                       let cad = fileMd;
                       //console.log(cad);
+                      //sacamos links
                       //let er1 = new RegExp ("(https*)+");
                       let er2 = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
                       let rta = er2.test(cad);
-                      console.log(rta);
-                      let arr = cad.match(er2);
+                      //console.log(rta);
+                      const arr = cad.match(er2);
                       console.log(arr);
-              });
+
+                       
+                     let reex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+                     let reaxtest = reex.test(arr.value);
+                     if(reaxtest == reaxtest){ 
+                     console.log(true);
+                     }
+                      const newArr = [];
+
+                  
+
+                    });
       } 
 
 
       });
    });
-  
-//buscando http con expresion regular
-
-//const http = require('http');
-
-  
-
-
-
-// let http = /^http+a/;
-// //let dir = new RegExp(/^http/);
-// //let newString = "fi";
-// if  (fileMd.test(http)){
-//     console.log("");
-// }
-
-
-
-
-// accede al fichero para lectura y lo devuelve uen string
-
-// accede al fichero para lectura y lo devuelve uen string
-// const fs = require('fs');
-// const path = require('path');
- 
-// fs.readdir('./(err,data');   {
-    
-//     if(path.extname() === ".md"){
-        
-//                  if(err)
-//                      console.log(err)
-//                  else
-//                      console.log(data);
-             
-//             };
-     
-//  };
-
-// const mdLinks = require("md-links");
-
-
-
-// // Ruta relativa con option (validate)
-// mdLinks("./some/example.md", { validate: true })
-//   .then(links => {
-//     // => [{ href, text, file, status, ok }]
-//   })
-//   .catch(console.error);
-
-
-//leer archivo linea por linea
-// const readline = require("readline");
-// const fs = require('fs');
-//     fileName = "README.md";
-
-// let lector = readline.createInterface({
-//     input: fs.createReadStream(fileName)
-// });
-
-// lector.on("line", line => {
-//     console.log("this is a line:", line);
-// });
-
-
-// function visible(a) {
-//     var R  =  ''
-//     for (var i = 0; i < a.length; i++) {
-//         if (a[i] == '\b') {  R -= 1; continue; }  
-//         if (a[i] == '\u001b') {
-//             while (a[i] != 'm' && i < a.length) i++
-//             if (a[i] == undefined) break
-//         }
-//         else R += a[i]
-//     }
-//     return  R
-// }
-
-// function empty(a) {
-//     a = visible(a)
-//     for (var i = 0; i < a.length; i++) {
-//         if (a[i] != ' ') return false
-//     }
-//     return  true
-// }
-
-// var readline = require('readline')
-// var rl = readline.createInterface({ input: process.stdin, output: process.stdout, terminal: false })
-
-// rl.on('line', function(line) {
-//     if (!empty(line)) console.log(line) 
-// })
-
-
-  //aqui estoy contando no se que cosa 
-//   cuenta = 0;
-//   posicion = datamd.indexOf("http");
-//   while ( posicion != -1 ) {
-//      cuenta++;
-//      posicion = datamd.indexOf("http",posicion+1);
-//      console.log(posicion);
-//   }
- 
-
-//(searchElement[, fromIndex])
-
-  //aqui estoy contando no se que cosa pero creo un loop
-//   cuenta = 0;
-//   posicion = datamd.toString("http");
-//   while ( posicion != -1 ) {
-//      cuenta++;
-//      posicion = datamd.toString("http",posicion+1);
-//      if (posicion.indexOf(datamd)) {
-//        posicion.push(datamd);
-//        console.log(datamd);
-
-//      }
-//   }
-// });
-
-
-// console.log(data)
-// fetch('README.md')
-// .then(res => res.json())
-// .then(load => {
-//   data = load;
-//   console.log(data)
-// })
-
-//const http = require('http');
-// const url = require('url');
-
-
-
-
-
-
-// const myURL = url.href('https://');
-
-// console.log(myURL.href);
-
-
-// let obtain = http(function (req, res) {
-//     let urlParts = url.parse(req.url, true),
-//         urlPathname = urlParts.pathname,
-//         body = '',
-//         reqInfo = {};
-
-//     req.on('data', function (data) {
-//         body += data; 
-//     });
-//     console.log(urlPathname)
-
-
-
-// });
-
-
-
-// const http = require('http');
-
-// const url = require('url');
-// const myURL = new URL('https:');
-// console.log(myURL.pathname);
-// // Prints /abc/xyz
-
-// myURL.pathname = '/abcdef';
-// console.log(myURL.href);
-// Prints https://example.org/abcdef?123
-
-
-// let links = document.links;
-// for(let i = 0; i < links.length; i++) {
-//   let linkHref = document.createTextNode(links[i].href);
-//    lineBreak = document.createElement("br");
-//   document.body.appendChild(linkHref);
-//   document.body.appendChild(lineBreak);
-//   console.log(linkHref);
-// }
-//debolver una coleccion de los elementos con atributo href
-
-
-
-// function UrlExists(url) {
-//   var http = new XMLHttpRequest();
-//   http.open('HEAD', url, false);
-//   http.send();
-//   return http.status!=404;
-// }
