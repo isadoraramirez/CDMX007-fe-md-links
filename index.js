21e2748f191e5e68022ea6f1809a178c44f9f6e7
doctoramalvada <mydemoni@hotmail.com>
2019-04-04 15:51:28 -0600

    quite codigo muerto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = () => {};
+
   const fs = require('fs');
   const path = require('path');
   
@@ -23,19 +23,39 @@ module.exports = () => {};
                       const arr = cad.match(er2);
                       console.log(arr);
 
-                       
+                       //expresion regular para links validos,da true?
                      let reex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
-                     let reaxtest = reex.test(arr.value);
-                     if(reaxtest == reaxtest){ 
-                     console.log(true);
-                     }
+                     let reaxtest = reex.test(arr);
+                     console.log(reaxtest);
+
+
+                    //  const valid = arr.match(reex);
+                    //  console.log(valid)
+                    //  //console.log(reaxtest);
+                    //  if(reaxtest == reaxtest){ 
+                    //  console.log(true);
+                    //  }else{
+                    //   console.log(err);
+                    //  }
                       const newArr = [];
+                    
+
+                  
+
+
+
+
 
                   
 
                     });
+                    
       } 
 
 
       });
    });
+
+ 
+
+   //module.exports.direct = direct;
--- path.js
+++ path.js
@@ -1,8 +1,3 @@
 //jalar la funcion export
 // const bye = require('./functions');//
 // console.log('hola');
-const http = require('http').createServer(webServer),
-path= require('path')
-urls=[
-    
-]
